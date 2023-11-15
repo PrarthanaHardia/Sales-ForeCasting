947d407a9779e668d0405b9c7471d3c402e4937e
PrarthanaHardia <prarthanahardia2017@gmail.com>
2023-11-15 23:45:50 +0530

    change in django

--- django/mysite/main/views.py
+++ django/mysite/main/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def index(response):
-    return render(response,"main/base.html",{})
+def signup(response):
+    return render(response,"main/signup.html",{})
   
+def index(response):
+    return render(response,"main/index.html",{})
+
+def login(response):
+    return render(response,"main/login.html",{})
+
+def home(response):
+    return render(response,"main/home.html",{})
